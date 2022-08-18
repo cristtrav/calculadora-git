466b2a315555144f6307a3f11912cd7382936512
Cristhian Traversi <crist.trav@gmail.com>
2022-08-18 17:31:35 -0400

    Se agrego la funcion de multiplicacion

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ function sumar(n1, n2){
     return n1+n2;
 }
 
+function multiplicar(n1, n2){
+    return n1 * n2;
+}
+
 let n1 = 10;
 let n2 = 50;
 console.log(`La suma de ${n1} + ${n2} = ${sumar(n1, n2)}`);
+console.log(`La multiplicacion de ${n1} x ${n2} = ${multiplicar(n1, n2)}`);
